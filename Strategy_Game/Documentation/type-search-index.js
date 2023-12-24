56990a1d30d2f10171d6da9152f1fdc161dabc1b
Pelmen149 <151387429+Pelmen149@users.noreply.github.com>
2023-12-24 15:50:11 +0300

    Версия 2.0 с исправленными недочётами

---
+++ Strategy_Game/Documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.example.strategy_game","l":"Game"},{"p":"com.example.strategy_game","l":"HelloApplication"},{"p":"com.example.strategy_game","l":"Main"}];updateSearchResults();
